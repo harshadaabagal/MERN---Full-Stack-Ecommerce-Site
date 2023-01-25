5e9fdb131a0fce3d6b7e860eaa060bef13d73a42
Harshada Bagal <bagal.h@northeastern.edu>
2023-01-25 13:35:11 -0500

    Created Product schema

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,14 @@
 //Connection to database
 const mongoose = require("mongoose") //Importing 
 
-mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true}).then((data)=>{
-    console.log(`Mongodb connected with server ${data.connection.host}`);
-}).catch((err)=>{
-    console.log(err);
-})
+const connectDatabase = () => {
+    // mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true})
+
+    mongoose.connect(process.env.DB_URI).then((data)=>{
+        console.log(`Mongodb connected with server ${data.connection.host}`);
+    }).catch((err)=>{
+        console.log(err);
+    })
+}
+
+module.exports = connectDatabase
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const app = require("./app") //import app from app.js
-
 const dotenv = require("dotenv")
+const connectDatabase = require("./config/database")
 
 //Config
 //To access the config.env file , we install dotenv
 dotenv.config({path:"backend/config/config.env"});
+
+//Connecting to database
+connectDatabase()
 
 //create a server
 app.listen(process.env.PORT, () => {
