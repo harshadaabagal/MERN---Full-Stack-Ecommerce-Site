7a5243aa27bccdb9bd746b0a7f0d0d7705000584
harshada1709 <bagal.h@northeastern.edu>
2023-01-30 17:18:23 -0500

    Removed redundant code

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,5 @@
-const Product = require("../models/productModel");
+const Product = require("../models/productModel")
 const ErrorHandler = require("../utils/errorhandler");
-
 
 //Create Product - Admin
 exports.createProduct = async (req, res, next) => {
@@ -27,13 +26,9 @@ exports.getAllProducts = async(req, res) => {
 
 exports.updateProduct = async(req,res) => {
    let product = await Product.findById(req.params.id)
-   if(!product)
-   {
-    return res.status(500).json({
-        success: false,
-        message: "Product not found"
-    })
-   } 
+   if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
    product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true,
 runValidators: true,
 useFindAndModify: false
@@ -48,12 +43,9 @@ res.status(200).json({
 exports.deleteProduct = async(req,res,next)=>
 {
     const product = await Product.findById(req.params.id);
-    if(!product) {
-        return res.status(500).json({
-            success:false,
-            message: "Product not found"
-        })
-    }
+    if (!product) {
+        return next(new ErrorHandler("Product not found", 404));
+      }
 
     await product.remove();
     res.status(200).json({
