a3250f215b54a990f2fac1bd827ac7b6159d1d99
harshada1709 <bagal.h@northeastern.edu>
2023-01-30 16:46:48 -0500

    updated files

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,7 @@
 const Product = require("../models/productModel")
 
 
-//Create Product
+//Create Product - Admin
 exports.createProduct = async (req, res, next) => {
 
     const product = await Product.create(req.body);
@@ -12,6 +12,66 @@ exports.createProduct = async (req, res, next) => {
 
 }
 
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({ message: "Route is working fine" })
+//Get All Product
+exports.getAllProducts = async(req, res) => {
+
+    const products = await Product.find();
+    res.status(200).json({
+        sucess: true,
+        products,
+     })
+}
+
+//Update Product - Admin
+
+exports.updateProduct = async(req,res) => {
+   let product = await Product.findById(req.params.id)
+   if(!product)
+   {
+    return res.status(500).json({
+        success: false,
+        message: "Product not found"
+    })
+   } 
+   product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true,
+runValidators: true,
+useFindAndModify: false
+});
+res.status(200).json({
+    success:true,
+    product
+})
+}
+
+//Delete Product
+exports.deleteProduct = async(req,res,next)=>
+{
+    const product = await Product.findById(req.params.id);
+    if(!product) {
+        return res.status(500).json({
+            success:false,
+            message: "Product not found"
+        })
+    }
+
+    await product.remove();
+    res.status(200).json({
+        success: true,
+        message: "Product Deleted Successfully"
+    })
+}
+
+//Get single Product details
+exports.getProductDetails = async(req,res,next)=>{
+    const product = await Product.findById(req.params.id);
+    if(!product) {
+        return res.status(500).json({
+            success:false,
+            message: "Product not found"
+        })
+    }
+    res.status(200).json({
+        success: true,
+        product
+    })
 }
