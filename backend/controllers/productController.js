f639e1e7f1a02d0e55b3cfa0175380990155587c
harshada1709 <bagal.h@northeastern.edu>
2023-02-02 15:11:09 -0500

    Search feature added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel")
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 //Create Product - Admin
 exports.createProduct = async (req, res, next) => {
@@ -25,7 +26,8 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 //Get All Product
 exports.getAllProducts = catchAsyncErrors(async(req, res) => {
 
-    const products = await Product.find();
+    const apiFeature = new ApiFeatures(Product.find(), req.query).search();
+    const products = await apiFeature.query;
     res.status(200).json({
         sucess: true,
         products,
