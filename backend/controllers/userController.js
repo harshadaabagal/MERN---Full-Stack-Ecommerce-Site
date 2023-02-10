db94c21f455f4d510833547b0f671c01a9469cd0
harshada1709 <bagal.h@northeastern.edu>
2023-02-09 23:36:47 -0500

    Created sendToken seperately

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
 
 // Register a User
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -16,13 +17,8 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
       },
     });
 
-    const token = user.getJWTToken();
-
-    res.status(201).json({
-        success: true,
-        user,
-        token
-    });
+    sendToken(user,201,res);
+   
 });
 
 // Login User
@@ -47,13 +43,7 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler("Invalid email or password", 401));
   }
 
-  const token = user.getJWTToken(); 
-
-  res.status(200).json({
-    success: true,
-    user,
-    token
-}); 
+  sendToken(user,200,res);
 });
 
   
