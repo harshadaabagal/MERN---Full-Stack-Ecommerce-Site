30daa050afbffcdb278f24452a3beec111bc90e0
harshada1709 <bagal.h@northeastern.edu>
2023-02-10 14:00:05 -0500

    Admin authorization added

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,8 +4,8 @@ const {isAuthenticatedUser, authorizeRoles} = require("../middleware/auth");
 
 const router = express.Router();
 
-router.route("/products").get(isAuthenticatedUser,authorizeRoles("admin"),getAllProducts);
-router.route("/product/new").post(isAuthenticatedUser,createProduct);
-router.route("/product/:id").put(isAuthenticatedUser,updateProduct).delete(isAuthenticatedUser,deleteProduct).get(isAuthenticatedUser,getProductDetails)
+router.route("/products").get(getAllProducts);
+router.route("/product/new").post(isAuthenticatedUser, authorizeRoles("admin"),createProduct);
+router.route("/product/:id").put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct).delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct).get(isAuthenticatedUser,getProductDetails)
 
 module.exports = router;
