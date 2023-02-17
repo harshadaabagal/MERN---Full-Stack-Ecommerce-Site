7bbd9d12bfc51b6744d80ec30711496b30f72d10
harshada1709 <bagal.h@northeastern.edu>
2023-02-17 00:21:54 -0500

    Update user profile

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const {
-    registerUser, loginUser, logout, forgotPassword, resetPassword,
+    registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile,
   } = require("../controllers/userController");
+
+  const {isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 
   const router = express.Router();
@@ -11,4 +13,7 @@ const {
   router.route("/password/forgot").post(forgotPassword);
   router.route("/password/reset/:token").put(resetPassword);
   router.route("/logout").get(logout);
+  router.route("/me").get(isAuthenticatedUser, getUserDetails);
+  router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+  router.route("/me/update").put(isAuthenticatedUser,updateProfile);
   module.exports = router;
